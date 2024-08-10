b501e2ce841b477ea69205ee6e1e02a45af8eff3
bautiilb <139379794+bautiilb@users.noreply.github.com>
2024-08-10 13:30:25 -0300

    1era corrida

---
+++ C#/P.O.O/ejerciciosObligatorios/ej1/Cuenta.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ej1
+{
+    public class Cuenta
+    {
+        string titular;
+        double cantidad;
+
+        public string Titular { get { return titular; } set { titular = value; } }
+        public double Cantidad { get { return cantidad; } set { cantidad = value; } }
+
+        public Cuenta(string T, double C)
+        {
+            this.Titular = T;
+            this.Cantidad = C;
+        }
+        public double Ingresar(double cant)
+        {
+            if (cant >= 0) { 
+                Cantidad += cant;
+            }
+            return Cantidad;
+        }
+        public double Retirar(double cant)
+        {
+            Cantidad -= cant;
+            if ((Cantidad - cant) < 0)
+            {
+                Cantidad = 0;
+            }
+            return Cantidad;
+        }
+    }
+}
---
+++ C#/P.O.O/ejerciciosObligatorios/ej1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ej1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Cuenta> cuentas = new List<Cuenta>();
+            Cuenta c1 = new Cuenta("Sebastian Yatra", 1209);
+            cuentas.Add(c1);
+            Cuenta c2 = new Cuenta("Sundar Pichai", 9405);
+            cuentas.Add(c2);
+            Cuenta c3 = new Cuenta("Carla Mendoza", 6388);
+            cuentas.Add(c3);
+
+
+        }
+    }
+}
