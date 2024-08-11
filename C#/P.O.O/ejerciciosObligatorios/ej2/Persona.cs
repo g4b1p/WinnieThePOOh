01c6e7ecaa62918df495942708aaca6e393c44ba
bautiilb <139379794+bautiilb@users.noreply.github.com>
2024-08-11 20:19:40 -0300

    Actualizaciones ejercicios

--- C#/P.O.O/ejerciciosObligatorios/ej2/Persona.cs
+++ C#/P.O.O/ejerciciosObligatorios/ej2/Persona.cs
@@ -8,16 +8,16 @@ namespace ej2
 {
     internal class Persona
     {
-        string sexo;
         string nombre;
         int edad;
+        char sexo;
         int dni;
         double peso;
         double altura;
 
-        public string Sexo { get { return sexo; } set { sexo = value; } }
         public string Nombre { get { return nombre; } set { nombre = value; } }
         public int Edad { get { return edad; } set { edad = value; } }
+        public char Sexo { get { return sexo; } set { sexo = value; } }
         public int DNI { get { return dni; } set { dni = value; } }
         public double Peso { get { return peso; } set { peso = value; } }
         public double Altura { get { return altura; } set { altura = value; } }
@@ -26,13 +26,13 @@ namespace ej2
         {
 
         }
-        public Persona(string N, int E, string S)
+        public Persona(string N, int E, char S)
         {
             this.Nombre = N;
             this.Edad = E;
             this.Sexo = S;
         }
-        public Persona(string N, int E, string S, int D, double P, double A)
+        public Persona(string N, int E, char S, int D, double P, double A)
         {
             this.Nombre = N;
             this.Edad = E;
@@ -44,7 +44,7 @@ namespace ej2
 
         public int CalcularIMC(double pes, double alt)
         {
-            double IMC = pes / (alt*alt);
+            double IMC = pes / (Math.Pow(alt, 2));
             if (IMC < 20)
             {
                 return -1;
@@ -69,12 +69,47 @@ namespace ej2
                 return false;
             }
         }
-        public void ComprobarSexo(string sex)
+        public void ComprobarSexo(char sex)
         {
-            if (sex == Sexo)
+            if (sex != 'H' && sex != 'M')
             {
-
+                Sexo = 'H';
             }
+        }
+        public void GenerarDNI()
+        {
+            Random r = new Random();
+            int dienai = r.Next(00000000,99999999);
+            DNI = dienai;
+        }
+        public void MostrarDetalles()
+        {
+            Console.WriteLine(Nombre);
+            Console.WriteLine(Edad);
+            Console.WriteLine(Sexo);
+            Console.WriteLine(DNI);
+            Console.WriteLine(Peso);
+            Console.WriteLine(Altura);
+        }
+        public void SetNombre(string N)
+        {
+            this.Nombre = N;
+        }
+        public void SetEdad(int E)
+        {
+            this.Edad = E;
+        }
+        public void SetSexo(char S)
+        {
+            this.Sexo = S;
+        }
+        public void SetPeso(double P)
+        {
+            this.Peso = P;
+        }
+        public void SetAltura(double A)
+        {
+            this.Altura = A;
         }
     }
 }
--- C#/P.O.O/ejerciciosObligatorios/ej2/Program.cs
+++ C#/P.O.O/ejerciciosObligatorios/ej2/Program.cs
@@ -10,6 +10,56 @@ namespace ej2
     {
         static void Main(string[] args)
         {
+            List<Persona> Personas = new List<Persona>();
+            Persona p1 = new Persona();
+            Persona p2 = new Persona("Juan Carlos IV", 45, 'H');
+            Persona p3 = new Persona("Maria Laura", 21, 'M', 48919226, 64, 1.72);
+            Personas.Add(p1);
+            Personas.Add(p2);
+            Personas.Add(p3);
+
+            Console.WriteLine("Ingrese los datos correspondientes en el siguiente orden: Nombre, Edad, Sexo, Peso, Altura");
+            p1.SetNombre(Console.ReadLine());
+            p1.SetEdad(int.Parse(Console.ReadLine()));
+            p1.SetSexo(char.Parse(Console.ReadLine()));
+            p1.GenerarDNI();
+            p1.SetPeso(double.Parse(Console.ReadLine()));
+            p1.SetAltura(double.Parse(Console.ReadLine()));
+
+            Console.WriteLine("Ingrese los datos correspondientes en el siguiente orden: Peso, Altura");
+            p2.GenerarDNI();
+            p2.SetPeso(double.Parse(Console.ReadLine()));
+            p2.SetAltura(double.Parse(Console.ReadLine()));
+
+            foreach(Persona p in Personas) {
+                if (p.CalcularIMC(p.Peso, p.Altura) == -1)
+                {
+                    Console.WriteLine($"{p.Nombre} Está en el peso ideal");
+                }
+                else if (p.CalcularIMC(p.Peso, p.Altura) == 1)
+                {
+                    Console.WriteLine($"{p.Nombre} Tiene sobrepeso");
+                }
+                else
+                {
+                    Console.WriteLine($"{p.Nombre} Tiene flacura");
+                }
+                if (p.EsMayorEdad(p.Edad) == true)
+                {
+                    Console.WriteLine($"{p.Nombre} Es mayor de edad");
+                }
+                else
+                {
+                    Console.WriteLine($"{p.Nombre} No es mayor de edad");
+                }
+                p.ComprobarSexo(p.Sexo);
+            }
+
+            p1.MostrarDetalles();
+            p2.MostrarDetalles();
+            p3.MostrarDetalles();
+
+            Console.ReadKey();
         }
     }
 }
