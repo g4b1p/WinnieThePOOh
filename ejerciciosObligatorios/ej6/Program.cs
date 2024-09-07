27cff94a7cedf85ccec91a2b0c0c9ee776bd74e0
bautiilb <139379794+bautiilb@users.noreply.github.com>
2024-09-07 01:01:32 -0300

    Actualización REDUX ej6

--- ejerciciosObligatorios/ej6/Program.cs
+++ ejerciciosObligatorios/ej6/Program.cs
@@ -13,17 +13,14 @@ namespace ej6
         static void Main(string[] args)
         {
             List<Libro> list = new List<Libro>();
-            Libro l1 = new Libro(29029891831233, "El visitante", "Stephen King", 592);
-            Libro l2 = new Libro(12901290930390, "IT", "Stephen King", 1504);
-
-            list.Add(l1);
-            list.Add(l2);
+            list.Add(new Libro(29029891831233, "El visitante", "Stephen King", 592));
+            list.Add(new Libro(12901290930390, "IT", "Stephen King", 1504));
 
             Libro aux = new Libro();
             foreach (Libro l in list)
             { 
                 l.MostrarDetalles();
-                if(l.NumPaginas > aux.NumPaginas)
+                if (l.NumPaginas > aux.NumPaginas)
                 {
                     aux.NumPaginas = l.NumPaginas;
                     aux.Titulo = l.Titulo;
