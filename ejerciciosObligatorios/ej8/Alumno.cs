b39f81addef3bc097bffb46f1fc7385fa74affa4
bautiilb <139379794+bautiilb@users.noreply.github.com>
2024-09-14 21:19:53 -0300

    Actualizo ej8

--- ejerciciosObligatorios/ej8/Alumno.cs
+++ ejerciciosObligatorios/ej8/Alumno.cs
@@ -6,7 +6,13 @@ using System.Threading.Tasks;
 
 namespace ej8
 {
-    internal class Alumno
+    internal class Alumno : Instituto
     {
+        int calificAtual;
+        public int CalificActual { get { return calificAtual; } set { calificAtual = value; } }
+        public Alumno(int C ,string N, int E, char S) : base(N, E, S)
+        {
+            calificAtual = C;
+        }
     }
 }
