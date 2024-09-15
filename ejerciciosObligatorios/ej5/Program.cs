eb264400816a8f2a48f9932591fef44ef1c304fd
bautiilb <139379794+bautiilb@users.noreply.github.com>
2024-09-14 21:14:09 -0300

    Arreglo ej5

--- ejerciciosObligatorios/ej5/Program.cs
+++ ejerciciosObligatorios/ej5/Program.cs
@@ -13,16 +13,16 @@ namespace ej5
         {
             List<Serie> series = new List<Serie>();
             List<Videojuego> juegos = new List<Videojuego>();
-            series.Add(new Serie("Madoka Magica", 3, "Mahou Shoujo", "Gen Urobochi"));
+            series.Add(new Serie("Madoka Magica", 5, "Mahou Shoujo", "Gen Urobochi"));
             series.Add(new Serie("Higurashi", 2, "Psycho", "Ryukishi07"));
-            series.Add(new Serie("Umineko", 2, "Murder Mystery", "Ryukishi07"));
-            series.Add(new Serie("Evangelion", 1, "Shounen", "Hideaki Anno"));
+            series.Add(new Serie("Umineko", 3, "Murder Mystery", "Ryukishi07"));
+            series.Add(new Serie("Evangelion", 2, "Shounen", "Hideaki Anno"));
             series.Add(new Serie("Lain", 1, "Phylo", "Yoshitoshi Abe"));
-
+            //preguntar al profe si la REDUXeacion debe ser si o si console.writeado
             juegos.Add(new Videojuego("Undertale", 6, "RPG", "Toby Fox"));
             juegos.Add(new Videojuego("Dying Light", 30, "Zombie Parkour RPG", "Techland"));
-            juegos.Add(new Videojuego("Yume Nikki", 5, "Puzzle", "kikiyama"));
-            juegos.Add(new Videojuego("Lobotomy Corporation", 10, "Management", "Project Moon"));
+            juegos.Add(new Videojuego("Yume Nikki", 4, "Puzzle", "kikiyama"));
+            juegos.Add(new Videojuego("Lobotomy Corporation", 20, "Management", "Project Moon"));
             juegos.Add(new Videojuego("Watch Dogs", 30, "Open World", "Ubisoft"));
 
             int cantS = 0;
@@ -40,7 +40,7 @@ namespace ej5
                 }
                 if (i < series.Count() - 1)
                 {
-                    if (series[i].CompareTo(series[i + 1]) == 1)
+                    if (series[i].CompareTo(lSerie) == 1)
                        lSerie = series[i];
                 }
                 else
@@ -57,7 +57,7 @@ namespace ej5
                 }
                 if (i < juegos.Count() - 1)
                 {
-                    if (juegos[i].CompareTo(juegos[i + 1]) == 1)
+                    if (juegos[i].CompareTo(lJuego) == 1)
                     {
                         lJuego = juegos[i];
                     }
