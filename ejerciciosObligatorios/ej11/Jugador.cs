2129cdd4325a7402055d48d62e4af21ab92809eb
chokoterisaki <47866874+chokoterisaki@users.noreply.github.com>
2024-09-19 21:07:42 -0300

    fix nombres?

---
+++ ejerciciosObligatorios/ej11/Jugador.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ej11
+{
+    internal class Jugador
+    {
+        string nombre;
+        int dinero;
+        public string Nombre { get { return nombre; } set { nombre = value; } }
+        public int Dinero { get { return dinero; } set {dinero = value; } }
+        Jugador(string nombre, int dinero) { this.nombre = nombre; this.dinero = dinero; }
+    }
+}
